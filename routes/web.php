aa37ba7ebe3ffa003519deb87f1d3cca104014f6
fernando <fernandovazquezcuevahirl@gmail.com>
2022-11-09 01:00:43 -0600

    traer datos del model al controlador hasta la vista desde la base de datos

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [UserController::class,'ObtenerDatos']);
+
+Route::get('/custom', function(){
+    $msj = 'mesaje desde routes web';
+    $hola = "hola";
+
+    $data = ['hola' => $hola, "adios" => "adios"];
+
+    return view('custom',$data,['mensaje' => $msj, "edad" => 15]);
+
 });
